307269b260b03198521b0221fc0bbda78bbd85fb
Smade War <smadewar@gmail.com>
2026-01-29 20:46:18 -0500

    initail commit

---
+++ src/main/java/com/Test/Test_project/Controller/TestController.java
@@ -0,0 +1,15 @@
+package com.Test.Test_project.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, My first Spring Boot Application!";
+    }
+
+
+}
